c93bf59d087933052ca98a919127d61a940adfca
Guigz <115234747+guigzlsx@users.noreply.github.com>
2023-05-26 21:45:09 +0200

    Update ajout.php

--- cours/ajout.php
+++ cours/ajout.php
@@ -2,6 +2,14 @@
 
 require_once 'database.php';
 
+// Vérifiez si l'utilisateur est déjà connecté
+// if (isset($_SESSION['username'])) {
+//     // Rediriger l'utilisateur vers la page d'accueil
+
+// } else {
+//     header('Location: connexion.php');
+//     exit();
+// }
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     if (
@@ -11,13 +19,24 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         if (
             !empty($_POST['titre']) && !empty($_POST['contenu'])
         ) {
-            $data = [
-                'titre' => $_POST['titre'],
-                'contenu' => $_POST['contenu']
-
+            $dataImage = [
+                'img' => 'img/' . $_FILES['img']['name'],
+                'img_file' => $_FILES['img']['tmp_name']
             ];
 
-            $request = $database->prepare("INSERT INTO articles (titre, contenu, date) VALUES (:titre, :contenu, NOW())");
+
+            $data = [
+                'titre' => $_POST['titre'],
+                'tag' => $_POST['tag'],
+                'contenu' => $_POST['contenu'],
+                'img' => $_FILES['img']['name']       
+            ];
+
+            move_uploaded_file($dataImage['img_file'], $dataImage['img']);
+
+            
+
+            $request = $database->prepare("INSERT INTO articles (titre, contenu, date, tag, img) VALUES (:titre, :contenu, NOW(), :tag, :img)");
             $request->execute($data);
             header("Location: index.php");
 
